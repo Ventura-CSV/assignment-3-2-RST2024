5154ed5f02c1716342540da5644013dbf99da644
RST2024 <rtanwani2024@gmail.com>
2024-06-26 11:28:08 -0700

    msg

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ def main():
     Code Your Program here
     ########################################
     """
+    minval = num1
+    maxval = num1
+    median = num1
+    if num1 < num2:
+        maxval = num2
+        if num3 < num1:
+            minval = num3
+            median = num1
+        if num2 < num3:
+            maxval = num3
+            median = num2
+    elif num1 > num2:
+        minval = num2
+        if num2 > num3:
+            minval = num3
+            median = num2
+    
+        
 
     print(minval, median, maxval)
     ########################################
