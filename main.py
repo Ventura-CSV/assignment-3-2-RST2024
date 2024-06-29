54c8f2505db8531bb0b3aec14ffcb262db7a790e
RST2024 <rtanwani2024@gmail.com>
2024-06-28 21:17:59 -0700

    msg

--- main.py
+++ main.py
@@ -8,27 +8,33 @@ def main():
     ########################################
     """
    
-    #Algorithm to find the max value
+    #Algorithm to find the min value
+    minval = num1
     maxval = num1
+    if num1 > num2:
+        minval = num2
+        if num2 > num3:
+            minval = num3
+    elif num1 > num3:
+        minval = num3
+    #Algorithm to find the max value
     if num1 < num2:
         maxval = num2
         if num2 < num3:
             maxval = num3
-        else:
-            maxval = num2
-        if num1 > num3:
-            minval = num3
-    else:
-        minval = num2
-        if num2 > num3:
-            minval = num3
-    if num1 < num3:
+    elif num1 < num3:
         maxval = num3
         
-        
+    #Algorithm to find the median
+    if num1 < num3 < num2 or num2 < num3 < num1:
+        median = num3
+    if num3 < num1 < num2 or num2 < num1 < num3:
+        median = num1
+    if num1 < num2 < num3 or num3 < num2 < num1:
+        median = num2
     
     
-        
+    
 
     print(minval, median, maxval)
     ########################################
